ce776d057606bd74c6f2cf5bceffd27d8c63db94
Vichisorn <james31366@gmail.com>
2022-02-21 03:29:49 +0000

    Enhance HTML forms.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script context="module" lang="ts">
   import type { Load } from "@sveltejs/kit";
+  import { enhance } from "$lib/actions/form";
+
   export const load: Load = async ({ fetch }) => {
     const res = await fetch("/todos.json");
     if (res.ok) {
@@ -21,6 +23,19 @@
   export let todos: Todo[];
 
   const title = "Todo";
+
+  const processNewTodoResult = async (res: Response, form: HTMLFormElement) => {
+    const newTodo = await res.json();
+    todos = [...todos, newTodo];
+    form.reset();
+  };
+  const processUpdatedTodoResult = async (res: Response) => {
+    const updatedTodo = await res.json();
+    todos = todos.map((t) => {
+      if (t.uid === updatedTodo.uid) return updatedTodo;
+      return t;
+    });
+  };
 </script>
 
 <svelte:head>
@@ -30,7 +45,12 @@
 <div class="todos">
   <h1>{title}</h1>
 
-  <form action="/todos.json" method="post" class="new">
+  <form
+    action="/todos.json"
+    method="post"
+    class="new"
+    use:enhance={{ result: processNewTodoResult }}
+  >
     <input
       type="text"
       name="text"
@@ -40,7 +60,13 @@
   </form>
 
   {#each todos as todo}
-    <TodoItem {todo} />
+    <TodoItem
+      {todo}
+      processDeletedTodoResult={() => {
+        todos = todos.filter((t) => t.uid !== todo.uid);
+      }}
+      {processUpdatedTodoResult}
+    />
   {/each}
 </div>
 
--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -18,12 +18,12 @@ export const api = (request: Request, data?: Record<string, unknown>) => {
       status = 201;
       break;
     case "DELETE":
-      todos = todos.filter(todo => todo.uid !== request.params.uid)
+      todos = todos.filter((todo) => todo.uid !== request.params.uid);
       status = 200;
       break;
 
     case "PATCH":
-      todos = todos.map(todo => {
+      todos = todos.map((todo) => {
         if (todo.uid === request.params.uid) {
           if (data.text) todo.text = data.text as string;
           else todo.done = data.done as boolean;
@@ -31,23 +31,27 @@ export const api = (request: Request, data?: Record<string, unknown>) => {
         return todo;
       });
       status = 200;
+      body = todos.find((todo) => todo.uid === request.params.uid);
       break;
 
     default:
       break;
   }
 
-  if (request.method.toUpperCase() !== "GET") {
+  if (
+    request.method.toUpperCase() !== "GET" &&
+    request.headers.get("accept") !== "application/json"
+  ) {
     return {
       status: 303,
       headers: {
-        location: "/"
-      }
+        location: "/",
+      },
     };
   }
 
   return {
     status,
-    body
-  }
-}
+    body,
+  };
+};
